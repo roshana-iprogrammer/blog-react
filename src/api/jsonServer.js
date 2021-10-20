5fa2f3796ad000cb7e116c07a7828e4766d36715
roshana-iprogrammer <roshan@iprogrammer.co>
2021-10-20 15:07:54 +0530

    final

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://e545-2401-4900-529a-aa6d-edd3-9f1d-c442-c217.ngrok.io'
+    baseURL: 'http://a599-2401-4900-529a-aa6d-edd3-9f1d-c442-c217.ngrok.io'
 })
 
